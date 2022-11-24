be93ad99176fc2eca7f9dc4b6e401c9de94fbad3
Busra <aydemirbusra097@gmail.com>
2022-11-24 03:38:39 +0300

    Add icon to sidenav items

--- src/app/layout/sidenav/sidenav-item.interface.ts
+++ src/app/layout/sidenav/sidenav-item.interface.ts
@@ -1,8 +1,10 @@
+import { Icon } from "@visurel/iconify-angular";
 
 export interface SidenavItem {
     route: string;
     label: string;
     routerLinkActive?: { exact: boolean };
+    icon: Icon;
     badge?: {
         value: string;
         background: string;
