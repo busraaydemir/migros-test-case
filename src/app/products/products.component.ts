671f74c71b540ad0550d566cfad5f9a277af8dfb
Busra <aydemirbusra097@gmail.com>
2022-11-24 03:29:55 +0300

    Add products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Basket } from '../core/api';
+import { Product } from '../core/api/product/product.model';
+import { ProductService } from '../core/api/product/product.service';
+import { ProductInBasketService } from '../core/api/productsInBasket/products-in-basket.service';
+import { IncreaseProductsCount } from '../core/store/basket';
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
+import { combineLatest } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,9 +16,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  basket: Basket;
+
+  constructor(
+    private productService: ProductService,
+    private productsInBasketService: ProductInBasketService,
+    private store: Store
+  ) { }
 
   ngOnInit(): void {
+    combineLatest([this.productService.list(), this.productsInBasketService.list()])
+      .pipe(untilDestroyed(this))
+      .subscribe(([products, baskets]) => {
+        this.products = [...products];
+        this.basket = baskets[0];
+      });
+  }
+
+  addBasket(product: Product) {
+    product.loading = true;
+
+    this.basket.products.push({ ...product, loading: false });
+    this.basket.productsCount = this.basket.productsCount + 1;
+    
+    this.productsInBasketService.update(this.basket.id, this.basket).subscribe(response => {
+      product.loading = false;
+      this.store.dispatch(new IncreaseProductsCount());
+    });
   }
 
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { ProductsComponent } from './products.component';
 import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @NgModule({
     declarations: [
         ProductsComponent
     ],
     imports: [
         CommonModule,
-        ProductRoutingModule
+        ProductRoutingModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatProgressSpinnerModule
     ]
 })
 export class ProductsModule { }
